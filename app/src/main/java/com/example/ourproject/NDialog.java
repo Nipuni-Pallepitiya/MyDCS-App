65c81dfc90ed0790c371809fcd3c90c002342525
Nipuni-Pallepitiya <npallepitiya@gmail.com>
2020-08-11 23:21:40 +0530

    finally please make this right

--- app/src/main/java/com/example/ourproject/NDialog.java
+++ app/src/main/java/com/example/ourproject/NDialog.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 
 
+
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 public class NDialog extends AppCompatDialogFragment {
