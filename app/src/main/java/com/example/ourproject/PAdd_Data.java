affc74718c32303e949447d13c75d4f6f3ce7572
Nipuni-Pallepitiya <npallepitiya@gmail.com>
2020-08-12 10:36:48 +0530

    Activity.main

--- app/src/main/java/com/example/ourproject/PAdd_Data.java
+++ app/src/main/java/com/example/ourproject/PAdd_Data.java
@@ -2,7 +2,11 @@ package com.example.ourproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class PAdd_Data extends AppCompatActivity {
@@ -10,9 +14,48 @@ public class PAdd_Data extends AppCompatActivity {
 
 
 
+
+    Button btn1,btn2,btn3;
+    ImageButton img;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p_add__data);
+
+        btn1 = findViewById(R.id.pbutton1);
+        btn2 = findViewById(R.id.pbutton2);
+        btn3 = findViewById(R.id.pbutton3);
+        img = findViewById(R.id.pimageButton1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PAdd_Data.this,Pdata_packages.class);
+                startActivity(i);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PAdd_Data.this,Pfixed_packages.class);
+                startActivity(i);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PAdd_Data.this,Pview_my_packages.class);
+                startActivity(i);
+            }
+        });
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PAdd_Data.this,MProfile.class);
+                startActivity(i);
+            }
+        });
     }
 }
