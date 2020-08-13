845a20b1f3014b70e9adfefe3154dd6dbafceaab
IT19207414 <it19207414@my.sliit.lk>
2020-08-13 22:24:26 +0530

    added chathu's part

--- app/src/main/java/com/example/ourproject/CCredit_card.java
+++ app/src/main/java/com/example/ourproject/CCredit_card.java
@@ -2,7 +2,10 @@ package com.example.ourproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CCredit_card extends AppCompatActivity {
 
@@ -11,4 +14,17 @@ public class CCredit_card extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c_credit_card);
     }
+
+    public void creditCard(View view){
+        Intent credit = new Intent(this, Ccreditcard2.class);
+        Button creditButton = (Button) findViewById(R.id.c_button18);
+        startActivity(credit);
+    }
+
+    public void cash(View view){
+        Intent cash = new Intent(this, CcashDetails.class);
+        Button caButton = (Button) findViewById(R.id.c_button20);
+        startActivity(cash);
+    }
+
 }
